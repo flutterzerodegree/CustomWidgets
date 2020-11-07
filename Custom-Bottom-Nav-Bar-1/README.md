21774a1512c52e85082c63fb7adf35f86615df66
ShiKuMi <49393139+shikumi-0@users.noreply.github.com>
2020-11-07 20:34:11 +0530

    added custom nav bar

---
+++ Custom-Bottom-Nav-Bar-1/README.md
@@ -0,0 +1,9 @@
+# Custom Bottom Nav Bar
+
+⭐️ this repo if you like it.
+
+## Getting Started 🚀
+
+- Clone the repo
+- Install the dependicies
+- Run it
---
+++ Custom-Bottom-Nav-Bar-1/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:custom_bottom_nav_bar/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
